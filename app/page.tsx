f937d4ba4b252a22780491b5c5c853b8115d49dd
chanjeong park <ckswjd9595@gmail.com>
2025-01-10 19:53:17 +0900

    Feat: tailwind로 초기 card container 만들기
    prettier formatting설정 하기

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <main>Hello world!</main>;
+  return (
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
+      <div className="bg-white w-full shadow-lg rounded-3xl p-5 max-w-screen-sm flex flex-col gap-3">
+        <input
+          className="bg-gray-200 h-12 w-full rounded-full pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button className="bg-black py-2 text-white rounded-full active:scale-90 outline-none transition-transform font-medium">
+          Search
+        </button>
+      </div>
+    </main>
+  );
 }
