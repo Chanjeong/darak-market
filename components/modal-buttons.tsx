57a62c3dedaa4efc5d4615dd097207e5f8ee36b9
chanjeong park <ckswjd9595@gmail.com>
2025-02-11 20:18:13 +0900

    Feat:모달 수정3

--- components/modal-buttons.tsx
+++ components/modal-buttons.tsx
@@ -25,7 +25,7 @@ export function ViewDetailButton() {
     <div>
       <button
         onClick={handleDetailsClick}
-        className="bg-amber-900 text-white p-3 rounded-lg hover:bg-amber-800 transition sm:p-px sm:text-xs">
+        className="bg-amber-900 text-white p-3 rounded-lg hover:bg-amber-800 transition ">
         자세히 보기
       </button>
     </div>
